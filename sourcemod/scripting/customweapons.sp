KoNLiG/customweapons

commit 39c78015a493629c7ff132c044dccd5b545045fe

Added model reissue for a third person

sourcemod/scripting/customweapons/api.sp
@@ -146,6 +146,7 @@ any Native_SetModel(Handle plugin, int numParams)
 		case CustomWeaponModel_World:
 		{
 			custom_weapon_data.world_model = source;
+			ReEquipWeaponEntity(entity);
 		}
 		case CustomWeaponModel_Dropped:
 		{

commit d63508c7a13670fa368f8149e11c0b05158716e8

Fixed a bug where a weapon animations were removed introduced in 39c78015a493629c7ff132c044dccd5b545045fe

sourcemod/scripting/customweapons.sp
@@ -156,7 +156,7 @@ void LateLoad()
 
 void ReEquipWeaponEntity(int weapon, int weapon_owner = -1)
 {
-	if (weapon_owner == -1 && (weapon_owner = GetEntPropEnt(weapon, Prop_Data, "m_hOwnerEntity")) == -1)
+	if ((weapon_owner == -1 && (weapon_owner = GetEntPropEnt(weapon, Prop_Data, "m_hOwnerEntity")) == -1) || !IsClientOwnWeapon(weapon_owner, weapon))
 	{
 		return;
 	}
@@ -185,3 +185,18 @@ void Frame_EquipWeapon(DataPack dp)
 	
 	dp.Close();
 } 
+
+bool IsClientOwnWeapon(int client, int weapon)
+{
+	int max_weapons = GetEntPropArraySize(client, Prop_Send, "m_hMyWeapons");
+	
+	for (int current_weapon, ent = -1; current_weapon < max_weapons; current_weapon++)
+	{
+		if ((ent = GetEntPropEnt(client, Prop_Send, "m_hMyWeapons", current_weapon)) != -1 && ent == weapon)
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}

sourcemod/scripting/customweapons/api.sp
@@ -146,6 +146,7 @@ any Native_SetModel(Handle plugin, int numParams)
 		case CustomWeaponModel_World:
 		{
 			custom_weapon_data.world_model = source;
+			
 			ReEquipWeaponEntity(entity);
 		}
 		case CustomWeaponModel_Dropped: